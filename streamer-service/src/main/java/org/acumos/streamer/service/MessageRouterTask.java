4363d7a13855ef85d0a1a5934859034a935af064
Gadde, Srikanth (sg453d) <sg453d@att.com>
2018-09-07 10:54:29 -0500

    DataStreamer
    Added License file
    Added .gitreview file
    Added swagger information and all the related files.
    Deleted the resources folder in streamer-catalog service.
    Added remaining files required with license text
    Made changes according to the review comments
    Added Mongo DB Install document
    Made changes according the comments recieved from the code review
    Added Java Doc for the interfaces
    
    Issue-ID: ACUMOS-1198
    
    Change-Id: I5d5a26dabd93c35db9365039f7ddc8b4a7540ece
    Signed-off-by: Gadde, Srikanth (sg453d) <sg453d@att.com>

---
+++ streamer-service/src/main/java/org/acumos/streamer/service/MessageRouterTask.java
@@ -0,0 +1,363 @@
+/*-
+ * ===============LICENSE_START=======================================================
+ * Acumos
+ * ===================================================================================
+ * Copyright (C) 2018 AT&T Intellectual Property. All rights reserved.
+ * ===================================================================================
+ * This Acumos software file is distributed by AT&T
+ * under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * This file is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ===============LICENSE_END=========================================================
+ */
+
+package org.acumos.streamer.service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.lang.invoke.MethodHandles;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+import org.json.simple.parser.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.acumos.streamer.common.DataStreamerUtil;
+import org.acumos.streamer.exception.DataStreamerException;
+
+@Service
+public class MessageRouterTask implements  Runnable {
+	
+	private static final String MODEL_VERSION = "modelVersion";
+
+	private static final String MODEL_KEY = "modelKey";
+
+	private static final String ATT_MODEL_VERSION = "ATT-ModelVersion";
+
+	private static final String ATT_MODEL_KEY = "ATT-ModelKey";
+
+	private static final String CODE_CLOUD_AUTHORIZATION2 = "CodeCloud-Authorization";
+
+	private static final String CODE_CLOUD_AUTHORIZATION = "codeCloudAuthorization";
+
+	private static final String AUTHORIZATION2 = "Authorization";
+
+	private static final String POST = "POST";
+
+	private static final String TEXT_PLAIN = "text/plain";
+
+	private static final String CONTENT_TYPE = "Content-Type";
+
+	private static final String PREDICTOR_URL = "predictorUrl";
+
+	private static final String POLLING_INTERVAL = "pollingInterval";
+
+	private static final String PUBLISHER_URL = "publisherUrl";
+
+	private static final String SUBSCRIBER_URL = "subscriberUrl";
+
+	private static final String CATALOG_KEY = "catalogKey";
+
+	private static final String MSG_ROUTER = "MsgRouter";
+
+	private static final String BASIC_AUTHORIZATION = "basic.authorization";
+	
+	private static final String NEWLINE = "\n";
+
+	@Autowired
+	private Environment env;
+	
+	private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+	
+	private final String  authorization = env.getProperty(BASIC_AUTHORIZATION);
+	private String catalogKey;
+	
+	public void run() {
+		
+     	log.info("MessageRouterTask::run: Begin");
+	        
+		JSONParser parser = new JSONParser();
+	     
+	    String results = null;
+	       
+		JSONArray catalogArray;
+		
+			try {
+				catalogArray = DataStreamerUtil.getCatalogsByCategory(authorization, MSG_ROUTER);
+				
+		        for (Object str : catalogArray.toArray()) {
+		        	try {
+			        	org.json.simple.JSONObject streamerCatalogObj = (org.json.simple.JSONObject) parser.parse(str.toString());
+		
+				        log.info("Loading catalog "+ streamerCatalogObj.get(CATALOG_KEY));
+		
+				        catalogKey =(String)streamerCatalogObj.get(CATALOG_KEY);
+			
+						//Step 2: Launch the Listener in new thread to retrieve data from the subscriber with pollingInterval.
+						String subscriberUrl = (String) streamerCatalogObj.get(SUBSCRIBER_URL);
+						String publisherUrl = (String) streamerCatalogObj.get(PUBLISHER_URL);
+						String[] responseDataFromSubscriberUrl = null;
+						int pollingInterval = 0;
+						try 
+						{
+							if(streamerCatalogObj.containsKey(POLLING_INTERVAL)) {
+							 pollingInterval = Integer.valueOf(streamerCatalogObj.get(POLLING_INTERVAL).toString());
+							}
+							responseDataFromSubscriberUrl = getMessageSubscriber(authorization,subscriberUrl,pollingInterval);
+						}
+						catch(Exception e) {
+							log.error("MessageRouterTask::Run: Failed to launch listener to retreive data from subscriberUrl: " + subscriberUrl);
+							throw new DataStreamerException("Thread failed to execute data retreival from subscriber: " + subscriberUrl);
+						}
+			
+						//Step 3: sends the data to predictor for scoring.
+						org.json.JSONObject predictDetails = getPredictDetails(catalogKey);
+						if(responseDataFromSubscriberUrl!=null) {
+							results = executePredictorScoring(authorization, streamerCatalogObj, responseDataFromSubscriberUrl,predictDetails);
+						}
+						publishResults(results,authorization, publisherUrl);
+			        	}catch(ParseException  | DataStreamerException |IOException  e) {
+			        		log.error("executePredictorScoring(),publishResults()::failed in either execute predictor or publishing results",e);	
+			        	}catch(Exception  e) {
+			        		log.error("executePredictorScoring(),publishResults()::failed in either execute predictor or publishing results",e);	
+			        	}
+		        }
+		        
+			}catch(Exception ex) {
+				log.error("run()::exception in MessageRouter Task. The thread continues:",ex);
+			}
+	}
+
+	protected String[] getMessageSubscriber(String Authorization, String subscriberUrl, Integer pollingInterval) 
+			throws IOException, DataStreamerException, InterruptedException, ExecutionException, TimeoutException, ParseException {
+        	log.info("MessageRouterTask::getMessageSubscriber: Begin");
+       
+			 List<String> responseData = new ArrayList<String>();
+			 int counter=0;
+			 while(true) {
+				    log.info("fetching data from subscriber: " + subscriberUrl);
+				    String[] responseDataLocal = DataStreamerUtil.getMsgsFromSubscriber(Authorization, subscriberUrl);
+					//if no response and counter is 0 then wait for polling interval
+				    if(responseDataLocal.length==0 && counter==0) {
+				    	TimeUnit.MINUTES.sleep(pollingInterval);
+						counter++;
+						continue;
+					}
+				    
+					for (String msg: responseDataLocal ) {
+						responseData.add(msg);
+						log.info("the msg  from for each loop: " + msg);
+					}
+					
+					break;
+					
+			 }
+			
+			if(responseData.isEmpty()) {
+				log.info("There is no Data to retrieve from the subscriber: " + subscriberUrl);
+			}
+				
+			String[] array = responseData.toArray(new String[responseData.size()]);	
+			log.info("The data from subscriber url is:" +array);
+			return array;
+		
+	}
+	protected String executePredictorScoring(String authorization,  
+			org.json.simple.JSONObject streamerCatalogObj, String[] responseData,org.json.JSONObject predictDetails) throws DataStreamerException, IOException {
+		log.info("MessageRouterTask::executePredictorScoring():: Begin");
+		String predictorUrl = streamerCatalogObj.get(PREDICTOR_URL).toString();
+		//String predictorUrl = "http://localhost:9091/com-att-cmlp/com_att_cmlp_m09286_20180817162844/v2/syncPredictions";
+		URL aUrl = null;
+		try {
+			log.info("Intiating URL connection to predictor");
+			aUrl = new URL(predictorUrl);
+		} catch (MalformedURLException e) {
+			log.error("MessageRouterTask::executePredictorScoring()::Encountered error while prearing URL for predictor",e);
+			throw new DataStreamerException("Encountered error while prearing URL for predictor");
+		}
+		HttpURLConnection aHttpURLConnection = null;
+		try {
+			log.info("MessageRouterTask::operateData()::opening HTTP connection");
+			aHttpURLConnection = (HttpURLConnection) aUrl.openConnection();
+		} catch (IOException e) {
+			log.error("executePredictorScoring()::Predictor HTTP connection opening failed",e);
+			throw new DataStreamerException("Predictor HTTP connection opening failed");
+		}
+		
+		aHttpURLConnection.setInstanceFollowRedirects(false);
+		aHttpURLConnection.setRequestProperty(CONTENT_TYPE, TEXT_PLAIN);
+		try {
+			log.info("MessageRouterTask::executePredictorScoring()::setting up method type for HTTP call");
+			aHttpURLConnection.setRequestMethod(POST);
+		} catch (ProtocolException e1) {
+			log.error("executePredictorScoring()::failed to intiate method type for HTTP call",e1);
+			throw new DataStreamerException("Failed to intiate method type for HTTP call to predictor");
+		}
+		aHttpURLConnection.setRequestProperty(AUTHORIZATION2, authorization);
+		
+		String codeCloudAuthorization = predictDetails.getString(CODE_CLOUD_AUTHORIZATION);
+	    aHttpURLConnection.setRequestProperty(CODE_CLOUD_AUTHORIZATION2, codeCloudAuthorization);
+	    aHttpURLConnection.setRequestProperty(ATT_MODEL_KEY, (String) streamerCatalogObj.get(MODEL_KEY));
+	    aHttpURLConnection.setRequestProperty(ATT_MODEL_VERSION, (String) streamerCatalogObj.get(MODEL_VERSION));
+
+		log.info("MessageRouterTask::executePredictorScoring()::sending predictor POST HTTP request");
+		aHttpURLConnection.setDoOutput(true);
+		OutputStream wr = null;
+		try {
+			wr =aHttpURLConnection.getOutputStream();
+		} catch (IOException e) {
+			log.error("executePredictorScoring()::Encountered error while intaiating o/p stream for HTTP connection.",e);
+			throw new DataStreamerException("Encountered error while intaiating o/p stream for HTTP connection");
+		}
+
+		StringBuffer strBuffer = new StringBuffer();
+		for(String response  :responseData ) {
+			
+			strBuffer.append(response);
+			strBuffer.append(NEWLINE);
+			
+		}
+		try {
+			wr.write(strBuffer.toString().getBytes());
+			wr.flush();
+			wr.close();
+		} catch (IOException e) {
+			log.error("executePredictorScoring() encountered error while writing incoming data to HTTP connection for scoring",e);
+			throw new DataStreamerException("Encountered error while writing incoming data to HTTP connection for scoring");
+		}
+
+		int responseCode = aHttpURLConnection.getResponseCode();
+
+		log.info("MessageRouterTask::executePredictorScoring()::the response from predictor with URL as: " + predictorUrl
+				+ " returned a rsponse as " + responseCode);
+		if ( responseCode > 299 ) {
+			String errorMessage = "";
+			try {
+				errorMessage = aHttpURLConnection.getResponseMessage();
+			} catch (IOException e) {
+				log.error("MessageRouterTask::executePredictorScoring()::Error getting the error response message", e);
+			}
+			log.error("MessageRouterTask::executePredictorScoring()::There was error during scoring. he preditor retuned a code: "
+					+ responseCode + ". The assocaed reuned message is " + errorMessage);
+			throw new DataStreamerException("There was error during scoring. the preditor retuned a code: " + responseCode
+					+ ". The assocaed reuned message is " + errorMessage);
+		}
+
+		BufferedReader in = null;
+		String inputLine;
+		StringBuffer responseFromScoring = new StringBuffer();
+		try {
+			in = new BufferedReader(new InputStreamReader(aHttpURLConnection.getInputStream()));
+			while ((inputLine = in.readLine()) != null) {
+				responseFromScoring.append(inputLine);
+				responseFromScoring.append(NEWLINE);
+				System.out.println(responseFromScoring);
+			}
+			in.close();
+		} catch (IOException e) {
+			log.error("MessageRouterTask::executePredictorScoring()::Error reading the response from predicor scoring", e);
+			throw new DataStreamerException("Error reading the response from predicor scoring");
+		}
+
+		log.info("MessageRouterTask::executePredictorScoring()::response message size is" + responseFromScoring.length());
+		
+		return responseFromScoring.toString();
+		
+	}
+	
+	protected void publishResults(String results,String authorization, String publisherUrl) throws DataStreamerException, IOException {
+				
+		URL aUrl = null;
+		try {
+			log.info("Intiating URL connection to Publisher");
+			aUrl = new URL(publisherUrl);
+		} catch (MalformedURLException e) {
+			log.error("MessageRouterTask::publishResults()::Encountered error while prearing URL for Publisher",e);
+		}
+		HttpURLConnection aHttpURLConnection = null;
+		try {
+			log.info("MessageRouterTask::publishResults()::opening HTTP connection");
+			aHttpURLConnection = (HttpURLConnection) aUrl.openConnection();
+		} catch (IOException e) {
+			log.info("MessageRouterTask::publishResults()::Publisher HTTP connection opening failed",e);
+		}
+		
+		aHttpURLConnection.setInstanceFollowRedirects(false);
+		aHttpURLConnection.setRequestProperty(CONTENT_TYPE, TEXT_PLAIN);
+		try {
+			log.info("MessageRouterTask::publishResults()::setting up method type for HTTP call");
+			aHttpURLConnection.setRequestMethod(POST);
+		} catch (ProtocolException e1) {
+			log.info("MessageRouterTask::publishResults()::failed to intiate method type for HTTP call",e1);
+		}
+		aHttpURLConnection.setRequestProperty(AUTHORIZATION2, authorization);
+		
+		log.info("MessageRouterTask::publishResults()::sending predictor POST HTTP request");
+		aHttpURLConnection.setDoOutput(true);
+				
+		OutputStream outputStream = aHttpURLConnection.getOutputStream();
+		outputStream.write(results.getBytes());
+		outputStream.flush();
+		outputStream.close();
+		
+		int responseCode = 300;
+		try {
+			responseCode = aHttpURLConnection.getResponseCode();
+		} catch (IOException e1) {
+			log.error("MessageRouterTask::publishResults()::Error getting the response code", e1.getMessage());
+		}
+
+		log.info("MessageRouterTask::publishResults()::the response from publisher with URL as: " + publisherUrl
+				+ " returned a rsponse as " + responseCode);
+		if ( responseCode > 299 ) {
+			String errorMessage = "";
+			try {
+				errorMessage = aHttpURLConnection.getResponseMessage();
+			} catch (IOException e) {
+				log.error("publishResults()::Error getting the error response message", e);
+			}
+			log.error("MessageRouterTask::publishResults()::There was error during scoring. the publisher retuned a code: "
+					+ responseCode + ". The associated returned message is " + errorMessage);
+		}
+		
+		log.info("In MessageRouterTask:publishResults() the prediction results are:"+results);
+		
+	}
+		
+	
+  protected org.json.JSONObject getPredictDetails(String catalogKey) {
+	  org.json.JSONObject predictorDetails = null;
+	  try {
+			log.info("MessageRouterTask::getPredictDetails()::fetching details of predictor");
+			predictorDetails = DataStreamerUtil.getPredictorDetails(authorization,catalogKey);
+						
+		
+		} catch (IOException | DataStreamerException e) {
+			log.error("getPredictDetails()::Encountered error while fetchng details of predictor :",e);
+		}
+	  
+	  return predictorDetails;
+
+  }	
+
+}
---
+++ streamer-service/src/main/java/org/acumos/streamer/service/PublisherImpl.java
@@ -0,0 +1,354 @@
+/*-
+ * ===============LICENSE_START=======================================================
+ * Acumos
+ * ===================================================================================
+ * Copyright (C) 2018 AT&T Intellectual Property. All rights reserved.
+ * ===================================================================================
+ * This Acumos software file is distributed by AT&T
+ * under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * This file is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ===============LICENSE_END=========================================================
+ */
+
+package org.acumos.streamer.service;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.invoke.MethodHandles;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+import java.net.URL;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.cxf.jaxrs.ext.multipart.Attachment;
+import org.apache.cxf.jaxrs.ext.multipart.MultipartBody;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.acumos.streamer.exception.DataStreamerException;
+
+
+public class PublisherImpl {
+
+	private static final String FILENAME = "filename";
+
+	private static final String BASIC = "Basic ";
+
+	private static final String LOCATION = "Location";
+
+	private static final String X_ATT_DR_PUBLISH_ID = "X-ATT-DR-PUBLISH-ID";
+
+	private static final String _100_CONTINUE = "100-Continue";
+
+	private static final String EXPECT = "Expect";
+
+	private static final String PUT = "PUT";
+
+	private static final String CONTENT_LENGTH = "Content-Length";
+
+	private static final String AUTHORIZATION = "Authorization";
+
+	private static final String FILE_SEPARATOR = "file.separator";
+
+	private static final String USER_DIR = "user.dir";
+
+	private static final String PUBLISH = "publish";
+
+	private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+	private String origurl;
+	private String auth;
+	private String redirfile = ".redir";
+	volatile private String url;
+	volatile private int redircnt;
+	volatile private int faildur;
+	volatile private long nexttry;
+
+	private void loadredir() throws IOException {
+		
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(redirfile));
+			String[] x = br.readLine().split("\t");
+			if (x.length == 3 && x[0].equals(origurl)) {
+				redircnt = Integer.parseInt(x[2]);
+				url = x[1];
+			}
+		} catch (Exception e) {
+			log.error("Exception in loadredir()" +e);
+		}
+		 finally {
+			 if(br != null ) {
+				 br.close(); 
+			 }			 
+		 }
+	}
+
+	private void saveredir() throws IOException {
+		
+		OutputStream os = null;
+		try {
+			os = new FileOutputStream(redirfile);
+			os.write((origurl + "\t" + url + "\t" + redircnt).getBytes());
+		
+		} catch (Exception e) {
+			log.error("Exception in PublisherImpl:saveredir()" +e.getMessage());
+		}
+		finally {
+			 if(os != null ) {
+				 os.close(); 
+			 }	
+		}
+	}
+
+	/**
+	 * Bump failure timeout on failure
+	 */
+	private synchronized void setfail() {
+		long now = System.currentTimeMillis();
+		if (now >= nexttry) {
+			nexttry = now + faildur;
+			faildur += faildur;
+			if (faildur > 3600000) {
+				faildur = 3600000;
+			}
+		}
+	}
+
+	/**
+	 * Reset failure timeout on success
+	 */
+	private synchronized void setsuccess() {
+		nexttry = 0;
+		faildur = 10000;
+		notifyAll();
+	}
+
+	/**
+	 * Wait for failure timeout to expire
+	 */
+	private synchronized void faildelay() {
+		long now = System.currentTimeMillis();
+		while (now < nexttry) {
+			try {
+				wait(nexttry + 100 - now);
+			} catch (Exception e) {
+				log.error("Exception in PublisherImpl:faildelay()" +e.getMessage());
+			}
+			now = System.currentTimeMillis(); // New fix added for retry if its
+												// failed.
+		}
+	}
+
+	private synchronized void clearredir(String saveurl) throws IOException {
+		if (url == saveurl) {
+			url = origurl;
+			redircnt = 0;
+			saveredir();
+		}
+	}
+
+	private synchronized String adjustredir(String saveurl, String location, String fileid) throws IOException {
+		if (url == saveurl) {
+			if (redircnt >= 2) {
+				url = origurl;
+				redircnt = 0;
+				saveredir();
+				return (null);
+			} else if (location.endsWith("/" + fileid)) {
+				String nurl = location.substring(0, location.length() - fileid.length() - 1);
+				if (nurl.equals(saveurl) || nurl.equals(origurl)) {
+					url = origurl;
+					redircnt = 0;
+					saveredir();
+					return (null);
+				}
+				url = nurl;
+				redircnt++;
+				saveredir();
+				return (location);
+			}
+		}
+		return (location);
+	}
+
+	public void deliver(String cadiAuth, String feedAuth, String xMetadata, MultipartBody attchedFiles) {
+
+		Attachment attachment = null;
+		String attachmentfilename = null;
+		String fileNameKey = null;
+		log.info("going to publish files");
+		String publishDirectory = PUBLISH;
+
+		for (int i = 0; i < attchedFiles.getAllAttachments().size(); i++) {
+			attachment = attchedFiles.getAllAttachments().get(i);			
+			attachmentfilename = attachment.getContentDisposition().getParameter(FILENAME);
+			File f = new File(System.getProperty(USER_DIR) + System.getProperty(FILE_SEPARATOR) + publishDirectory
+					+ System.getProperty(FILE_SEPARATOR) + attachmentfilename);
+			while (attempt(f, attachmentfilename)) {
+			}
+		}
+	}
+
+	private boolean attempt(File f, String fid) {
+		byte[] buf = new byte[65536];
+		boolean reschedule = false;
+		long flen = f.length();
+		try {
+			faildelay();
+			String saveurl = url;
+			URL u = new URL(saveurl + "/" + fid);
+			HttpURLConnection uc = (HttpURLConnection) u.openConnection();
+			uc.setInstanceFollowRedirects(false);
+			if (auth != null) {
+				uc.setRequestProperty(AUTHORIZATION, auth);
+			}
+			boolean connectok = false;
+			int rc = -1;
+			String responsemessage = null;
+			String lochdr = null;
+			String pubid = null;
+			try {
+				uc.setFixedLengthStreamingMode(flen);
+				uc.setRequestProperty(CONTENT_LENGTH, Long.toString(flen));
+				uc.setRequestMethod(PUT);
+				uc.setRequestProperty(EXPECT, _100_CONTINUE);
+				uc.setDoOutput(true);
+				long sofar = 0;
+				OutputStream os = null;
+				try {
+					uc.connect();
+					os = uc.getOutputStream();
+					connectok = true;
+				} catch (ProtocolException pe) {
+					// Rcvd error instead of 100-Continue
+					try {
+						// work around glitch in Java 1.7.0.21 and likely others
+						// without this, Java will connect multiple times to the
+						// server to run the same request
+						uc.setDoOutput(false);
+					} catch (Exception e) {
+						log.error("Exception in PublisherImpl:attempt()" +e.getMessage());
+					}
+				}
+				if (os != null) {
+					try {
+						InputStream is = new FileInputStream(f);
+						while (sofar < flen) {
+							int len = buf.length;
+							if (flen - sofar < len) {
+								len = (int) (flen - sofar);
+							}
+							len = is.read(buf, 0, len);
+							os.write(buf, 0, len);
+							sofar += len;
+						}
+						is.close();
+						os.close();
+					} catch (Exception e) {
+						log.error("Exception in PublisherImpl:attempt():Error publishing file id " + fid);
+					}
+				}
+				rc = uc.getResponseCode();
+				responsemessage = uc.getResponseMessage();
+				if (responsemessage == null) {
+					// work around for glitch in Java 1.7.0.21 and likely others
+					// When Expect: 100 is set and a non-100 response is
+					// received, the response message is not set but the
+					// response code is
+					String h0 = uc.getHeaderField(0);
+					if (h0 != null) {
+						int i = h0.indexOf(' ');
+						int j = h0.indexOf(' ', i + 1);
+						if (i != -1 && j != -1) {
+							responsemessage = h0.substring(j + 1);
+						}
+					}
+				}
+				if (rc >= 200 && rc < 300) {
+					pubid = uc.getHeaderField(X_ATT_DR_PUBLISH_ID);
+				} else if (rc >= 300 && rc < 400) {
+					lochdr = uc.getHeaderField(LOCATION);
+				}
+				connectok = true;
+			} catch (Exception e) {
+				log.error("Exception in PublisherImpl:attempt():Error publishing file id " + fid);
+			}
+			try {
+				// discard input so can re-use connection
+				InputStream is = (rc >= 300) ? uc.getErrorStream() : uc.getInputStream();
+				is.close();
+			} catch (Exception e) {
+				log.error("Exception in PublisherImpl:attempt()" + e.getMessage());
+			}
+			if (redircnt > 0 && !connectok) {
+				// URL had been redirected but replacement URL failed - go back
+				// to original URL
+				log.error("Error connecting to publish after redirection - reverting to original URL");
+				clearredir(saveurl);
+				reschedule = true;
+				setfail();
+				return (reschedule);
+			}
+			if (rc >= 200 && rc < 300) {
+				// 2xx success
+				log.info("Published file id " + fid + " to " + saveurl + " publish ID " + pubid);
+				reschedule = false;
+				setsuccess();
+				return (reschedule);
+			}
+			if (rc >= 300 && rc < 400) {
+				// 3xx redirect
+				log.info("Publish attempt to " + saveurl + " redirected (" + lochdr + ").");
+				String nloc = adjustredir(saveurl, lochdr, fid);
+				reschedule = true;
+				if (nloc == null) {
+					setfail();
+				}
+				return (reschedule);
+			}
+			if (rc >= 400 && rc < 500) {
+				// 4xx Bad request
+				log.error("Publish attempt for " + fid + " to " + saveurl + " permanently rejected code " + rc
+						+ " - " + responsemessage);
+				setfail();
+				return (reschedule);
+			}
+			// 5xx, can't connect, or unparsable response - Server problem
+			log.error("Publish attempt for " + fid + " to " + saveurl + " temporarily rejected code " + rc
+					+ " - " + responsemessage);
+			setfail();
+			reschedule = true;
+			return (reschedule);
+		} catch (Exception e) {
+			log.error("Exception in PublisherImpl:attempt():Unexpected error during publish attempt " + e.getMessage());
+			return (reschedule);
+		}
+	}
+
+	public String doPublish(String cadiAuth, String feedAuth, String xMetadata, MultipartBody file) throws Exception {
+
+		if (feedAuth.split(":").length != 2) {
+			throw new DataStreamerException("the format of feedAuth header should be user:password");
+		}
+		auth = BASIC + Base64.encodeBase64String(feedAuth.getBytes());
+
+		deliver(cadiAuth, auth, xMetadata, file);
+		return null;
+	}
+
+}
